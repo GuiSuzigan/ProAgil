525470c8d5a5acb72b3b4a793295f8c57964c187
Suzigan1 <guilhermesuzigan@hotmail.com>
2020-06-01 18:06:42 -0300

    Inicial do meu projeto de Eventos

---
+++ ProAgil.WebApi/Migrations/20200601190613_qualquerCoisa.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProAgil.WebApi.Migrations
+{
+    public partial class qualquerCoisa : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
